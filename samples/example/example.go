afd4496a4928466ac2c32041a6b40523ad94d541
anatolypaw <anatolypaw@gmail.com>
2024-02-14 19:45:20 +0600

    fix

--- samples/example/example.go
+++ samples/example/example.go
@@ -18,20 +18,17 @@ func main() {
 	rect := image.Rect(0, 0, 800, 480)
 	display := image.NewRGBA(rect)
 
-	// Создаем устройство ввода
-
 	// Создаем гуй
 	gui, err := sgui.New(display, nil)
 	if err != nil {
 		panic(err)
 	}
 	backcolor := color.RGBA{50, 50, 50, 255}
-	//gui.SetBackground(backcolor)
+	gui.SetBackground(backcolor)
 
 	for i := 0; i < 5; i++ {
 		for n := 0; n < 10; n++ {
 			// Создаем виджеты
-
 			ind := widget.NewIndicator(20, backcolor)
 			ind.AddState(color.RGBA{255, 0, 0, 255})
 			ind.AddState(color.RGBA{0, 255, 0, 255})
@@ -50,7 +47,7 @@ func main() {
 			},
 				backcolor)
 
-			// Добавляем виджеты
+			// Добавляем виджеты на холст
 			gui.AddWidget(10+i*160, 10+(n*47), button)
 			gui.AddWidget(130+i*160, 20+(n*47), ind)
 
@@ -62,6 +59,7 @@ func main() {
 	gui.Render()
 	log.Printf("Rendering  %v\n", time.Since(start))
 
+	// Сохраняем рендер в файл
 	f, err := os.Create("example.png")
 	if err != nil {
 		return
